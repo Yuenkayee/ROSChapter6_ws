057d7497556af778afa23515212ee316d6600c07
yuenkayee <2676904949@qq.com>
2026-03-12 13:52:28 +0800

    initialized position setteled

---
+++ src/yuen_app/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'yuen_app'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='2676904949@qq.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "init_robot_pose=yuen_app.init_robot_pose:main"
+        ],
+    },
+)
